3473151495e1e8754d1d41078f36558cc04ebc1f
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-08-19 04:41:14 +0530

    Process Data Models
    Add Association on models
    Add CRUD methonds on models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_comment_counter
+
+  def update_comment_counter
+    post.update(comments_counter: (post.comments_counter || 0) + 1)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  has_many :likes
+  has_many :comments
+
+  def most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
