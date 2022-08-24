8f137eaceee3711d35ca4885e3b78bb5959a9373
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-08-24 08:00:10 +0530

    Setup Controller Add views Add routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.where(user_id: params[:user_id])
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def show
+    @values = params
+    @post = Post.find_by(user_id: params[:user_id], id: params[:id])
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @posts = Post.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.permit(:id)
+  end
+end
