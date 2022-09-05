86870a3b3d4f81b8861806c716bb545b743d04f2
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-09-05 09:36:47 +0530

    Add rails 12factor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "PenNode_production"
 
-  config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_caching = true
+  config.action_mailer.default_url_options = { :host => "pennode-verse.herokuapp.com"}
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
